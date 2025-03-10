b34b1b8fe6fd0416093e52e2b3ebfc3db8eb8535
Pim van Milt <pim.milt@gmail.com>
2025-03-10 11:11:12 +0100

    blah

--- resources/scripts/objects/Killzone.cs
+++ resources/scripts/objects/Killzone.cs
@@ -11,12 +11,15 @@ public partial class Killzone : Area2D
     private void _on_body_entered(Node body)
     {
         GD.Print("You died");
+        body.GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
+        Engine.TimeScale = 0.5;
         timer.Start();
 
     }
 
     private void _on_timer_timeout()
     {
+        Engine.TimeScale = 1.0;
         GetTree().ReloadCurrentScene();
     }
 }
